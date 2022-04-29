9e7004bf2de50fcee5381b912bc5fb380e7952df
Andre Chen <andrec279@gmail.com>
2022-04-29 11:21:50 -0400

    spark refactor v1 complete

--- split_code_HDFS.py
+++ split_code_HDFS.py
@@ -19,7 +19,7 @@ import random
 import math
 
 def main(spark, netID):
-
+    t0 = time.time()
     if local_save==True:
         path = 'ml-latest{}/'.format(size)
     else:
@@ -36,10 +36,7 @@ def main(spark, netID):
         
     # Read in and add temporary row_id column for splitting
     df_ratings = spark.read.csv(files['ratings'], header='true')
-    df_ratings = df_ratings.withColumn('row_id', monotonically_increasing_id())    
-    
-    print('Original df:')
-    df_ratings.show()
+    df_ratings = df_ratings.withColumn('row_id', monotonically_increasing_id())
     
     # define function for random sampling from each userId
     def sample(iter, frac): 
@@ -51,15 +48,13 @@ def main(spark, netID):
     user_groups = df_ratings.rdd.map(lambda row: (row.userId, row)).groupByKey()
     
 
+    '''SPLITTING SECTION'''
     # 60/20/20 Train/Val/Test Split on individual user basis using df.groupby.sample()
-    # NOTE: Data is split such that earlier interactions are in training data, later interactions
-    # are split randomly between validation and test data
+    print('Splitting into train / val /test...')
     
-    t0 = time.time()
     
     train_frac = 0.6
     ratings_train = spark.createDataFrame(user_groups.flatMap(lambda r: sample(r[1], train_frac))).persist()
-    
     
     # Filter training data out of full dataset
     ratings_val_test = df_ratings.join(ratings_train, 'row_id', 'left_anti')
@@ -70,124 +65,51 @@ def main(spark, netID):
     # Sample 50% of remainder (ratings_val_test) to get validation split
     ratings_val = spark.createDataFrame(val_test_user_groups.flatMap(lambda r: sample(r[1], val_frac))).persist()
 
-    
-    # Filter validation split out of ratings_val_test to get test split
+    # Remove validation split from ratings_val_test, remainder is the test split
     ratings_test = ratings_val_test.join(ratings_val, 'row_id', 'left_anti')
     
+    # Remove row_id, we only needed it for splitting
+    ratings_train = ratings_train.select('userId', 'movieId', 'rating', 'timestamp')
+    ratings_val = ratings_val.select('userId', 'movieId', 'rating', 'timestamp')
+    ratings_test = ratings_test.select('userId', 'movieId', 'rating', 'timestamp')
+    print('Splitting done')
+
+    '''FILTERING SECTION'''    
     # Filter in validation / test to only users with 5+ reviews
     threshold = 5
+    print('Filtering validation and test to users with 5+ reviews and only movies in training set...')
     
-    ratings_val_ct = ratings_val.groupBy('userId').agg(F.count('movieId').alias('movieCt')).filter(col('movieCt')>=threshold)
-    print('ratings_val_ct')
-    ratings_val_ct.show()
+    windowSpecCount = Window.partitionBy('userId')
+    ratings_val = ratings_val.withColumn('movieCt', F.count(col('movieId')).over(windowSpecCount))
+    ratings_test = ratings_test.withColumn('movieCt', F.count(col('movieId')).over(windowSpecCount))
+    
+    ratings_val = ratings_val.filter(ratings_val['movieCt']>=threshold)
+    ratings_test = ratings_test.filter(ratings_test['movieCt']>=threshold)
     
     
-    ratings_val = ratings_val.groupBy('userId').filter(lambda x: len(x) >= threshold)
-    ratings_test = ratings_test.groupBy('userId').filter(lambda x: len(x) >= threshold)
+    # Filter validation / test to only movies that are in training set
+    train_movies = set(ratings_train.select('movieId').rdd.map(lambda row: row['movieId']).collect())
+    ratings_val = ratings_val.filter(col('movieId').isin(train_movies))
+    ratings_test = ratings_test.filter(col('movieId').isin(train_movies))
     
-    training_movies = ratings_train.select('movieId').collect()
-    print(training_movies)
+    print('Filtering done')
+    
+    if local_save == True:
+        filepath = ''
+    
+    else:
+        filepath = f'hdfs:/user/{netID}/'
+    
+    ratings_train.write.mode('overwrite').option('header', True).parquet(filepath + f'ratings_train{size}.parquet')
+    ratings_val.write.mode('overwrite').option('header', True).parquet(filepath + f'ratings_val{size}.parquet')
+    ratings_test.write.mode('overwrite').option('header', True).parquet(filepath + f'ratings_test{size}.parquet')
 
-    # Checks
-
-    # Run the following filters and checks to ensure that:
-    # 1. All users in the training set appear in the validation and test set 
-    #    (Note: There is no overlap in interactions data between the three splits)
-    # 2. All movies in the training set appear in the validation and test set 
-    # 3. There are enough ratings per person (at least 4?) in all three splits
-    # 4. There is enough data overall in all three splits
-
-
-    print('''====================
-    Before Filtering:
-    ====================\n''')
+    print('\nFiles written successfully to {}'.format('local folder' if local_save == True else filepath))
+    print('\nTest output for ratings_train:')
+    print(spark.read.parquet(filepath + f'ratings_train{size}.parquet', header='true').show(10))
     
-    # print('Total DF len:', len(df_ratings.index))
-    # print('Train DF len:', len(ratings_train.index))
-    # print('Val DF len:', len(ratings_val.index))
-    # print('Test DF len:', len(ratings_test.index))
+    print('Full runtime: ', time.time()-t0, 'seconds')
     
-    # print('\nNumber of users in each split:')
-    # print('Total:', len(pd.unique(df_ratings['userId'])))
-    # print('Train:', len(pd.unique(ratings_train['userId'])))
-    # print('Val:', len(pd.unique(ratings_val['userId'])))
-    # print('Test:', len(pd.unique(ratings_test['userId'])))
-    
-    # all_movie_ids = pd.unique(df_ratings['movieId'])
-    # train_movie_ids = pd.unique(ratings_train['movieId'])
-    # val_movie_ids = pd.unique(ratings_val['movieId'])
-    # test_movie_ids = pd.unique(ratings_test['movieId'])
-    
-    # print('\nNumber of movies in each split:')
-    # print('Total:', len(all_movie_ids))
-    # print('Train:', len(train_movie_ids))
-    # print('Val:', len(val_movie_ids))
-    # print('Test:', len(test_movie_ids))
-    
-    
-    # # Find rows corresponding movies in val and test splits that are 
-    # # NOT in train split and remove
-    
-    # val_missing_from_train = [movieId for movieId in val_movie_ids if movieId not in train_movie_ids]
-    # test_missing_from_train = [movieId for movieId in test_movie_ids if movieId not in train_movie_ids]
-    
-    # ratings_val_filtered = ratings_val[~ratings_val['movieId'].isin(val_missing_from_train)]
-    # ratings_test_filtered = ratings_test[~ratings_test['movieId'].isin(test_missing_from_train)]
-    
-    
-    # print('''\n====================
-    # After Filtering:
-    # ====================\n''')
-    
-    # print('Total DF len:', len(df_ratings.index))
-    # print('Train DF len:', len(ratings_train.index))
-    # print('Val DF len:', len(ratings_val_filtered.index))
-    # print('Test DF len:', len(ratings_test_filtered.index))
-    
-    # print('\nNumber of users in each split:')
-    # print('Total:', len(pd.unique(df_ratings['userId'])))
-    # print('Train:', len(pd.unique(ratings_train['userId'])))
-    # print('Val:', len(pd.unique(ratings_val_filtered['userId'])))
-    # print('Test:', len(pd.unique(ratings_test_filtered['userId'])))
-    
-    # print('\nNumber of movies in each split:')
-    # print('Total:', len(pd.unique(df_ratings['movieId'])))
-    # print('Train:', len(pd.unique(ratings_train['movieId'])))
-    # print('Val:', len(pd.unique(ratings_val_filtered['movieId'])))
-    # print('Test:', len(pd.unique(ratings_test_filtered['movieId'])))
-    
-    # print('\nMinimum Ratings per User:')
-    # print('Train:', ratings_train.groupby('userId')['movieId'].count().min())
-    # print('Validation:', ratings_val.groupby('userId')['movieId'].count().min())
-    # print('Test:', ratings_test.groupby('userId')['movieId'].count().min())
-
-    # # Convert to spark to write to HDFS
-    # cols = ['userId', 'movieId', 'rating', 'timestamp']
-    
-    # if local_save == True:
-    #     ratings_train[cols].to_csv('ratings_train{}.csv'.format(size))
-    #     ratings_val[cols].to_csv('ratings_val{}.csv'.format(size))
-    #     ratings_test[cols].to_csv('ratings_test{}.csv'.format(size))
-        
-    #     print('\nTrain/Validation/Split files written successfully to local folder')
-    #     print('\nTest output for ratings_train:')
-    #     print(pd.read_csv(f'ratings_train{size}.csv').head(10))
-    
-    # else:
-    
-    #     ratings_train = spark.createDataFrame(ratings_train[cols])
-    #     ratings_val = spark.createDataFrame(ratings_val[cols])
-    #     ratings_test = spark.createDataFrame(ratings_test[cols])
-        
-    #     ratings_train.write.mode('overwrite').option("header",True).csv(f'hdfs:/user/{netID}/ratings_train{size}.csv')
-    #     ratings_val.write.mode('overwrite').option("header",True).csv(f'hdfs:/user/{netID}/ratings_val{size}.csv')
-    #     ratings_test.write.mode('overwrite').option("header",True).csv(f'hdfs:/user/{netID}/ratings_test{size}.csv')
-    
-    #     print('\nTrain/Validation/Split files written successfully to HDFS')
-    #     print('\nTest output for ratings_train:')
-    #     print(spark.read.csv(f'hdfs:/user/{netID}/ratings_train{size}.csv', header='true').show(10))
-    
-
 # Only enter this block if we're in main
 if __name__ == "__main__":
     # Create the spark session object
@@ -195,7 +117,7 @@ if __name__ == "__main__":
 
     local_save = True # Set = True for debugging on local machine
     
-    full_data = False
+    full_data = True
     size = '-small' if full_data==False else ''
     
     if local_save == False:
