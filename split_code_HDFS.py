c75fe649bd8e2d015aea0debb1bb94cf362de500
Andre Chen <andrec279@gmail.com>
2022-05-05 16:11:32 -0400

    added time-based splitting logic

--- split_code_HDFS.py
+++ split_code_HDFS.py
@@ -36,7 +36,11 @@ def main(spark, netID):
         
     # Read in and add temporary row_id column for splitting
     df_ratings = spark.read.csv(files['ratings'], header='true')
+    
+    # Add percentile column, ordered by timestamp and row_id column for splitting
+    user_window_time = Window.partitionBy('userId').orderBy(F.col('timestamp'))
     df_ratings = df_ratings.withColumn('row_id', monotonically_increasing_id())
+    df_ratings = df_ratings.withColumn('time_pct', F.percent_rank().over(user_window_time))
     
     # define function for random sampling from each userId
     def sample(iter, frac): 
@@ -45,30 +49,34 @@ def main(spark, netID):
         rs = random.Random()
         return rs.sample(list(iter), n)  
     
-    user_groups = df_ratings.rdd.map(lambda row: (row.userId, row)).groupByKey()
     
 
     '''SPLITTING SECTION'''
     # 60/20/20 Train/Val/Test Split on individual user basis using df.groupby.sample()
     print('Splitting into train / val /test...')
     
+    # Get 40% earliest interactions and 20% latest interactions per user for training set
+    train_lower_frac = 0.4
+    train_upper_frac = 0.8
     
-    train_frac = 0.6
-    ratings_train = spark.createDataFrame(user_groups.flatMap(lambda r: sample(r[1], train_frac))).persist()
+    # Get training split
+    ratings_train = df_ratings.filter((df_ratings['time_pct']<=train_lower_frac)|(df_ratings['time_pct']>train_upper_frac))\
+                              .persist()
     
-    # Filter training data out of full dataset
+    # Filter out ratings_train from df_ratings
+    # Remaining 40% of each user's data split evenly between val and test
     ratings_val_test = df_ratings.join(ratings_train, 'row_id', 'left_anti')
     
-    val_test_user_groups = ratings_val_test.rdd.map(lambda row: (row.userId, row)).groupByKey()
-    val_frac = 0.5
     
     # Sample 50% of remainder (ratings_val_test) to get validation split
-    ratings_val = spark.createDataFrame(val_test_user_groups.flatMap(lambda r: sample(r[1], val_frac))).persist()
+    val_frac = 0.5
+    user_groups = ratings_val_test.rdd.map(lambda row: (row.userId, row)).groupByKey()
+    ratings_val = spark.createDataFrame(user_groups.flatMap(lambda r: sample(r[1], val_frac))).persist()
 
     # Remove validation split from ratings_val_test, remainder is the test split
     ratings_test = ratings_val_test.join(ratings_val, 'row_id', 'left_anti')
     
-    # Remove row_id, we only needed it for splitting
+    # Remove row_id / timestamp pctile columns, we only needed them for splitting
     ratings_train = ratings_train.select('userId', 'movieId', 'rating', 'timestamp')
     ratings_val = ratings_val.select('userId', 'movieId', 'rating', 'timestamp')
     ratings_test = ratings_test.select('userId', 'movieId', 'rating', 'timestamp')
@@ -94,6 +102,29 @@ def main(spark, netID):
     
     print('Filtering done')
     
+    # '---FOR DEBUGGING---'
+    
+    # print('\nSplit sizes:')
+    # total_len = df_ratings.count()
+    # train_count = ratings_train.count()
+    # val_count = ratings_val.count()
+    # test_count = ratings_test.count()
+    # print('Training COUNT: {} Training PCT: {}'.format(train_count, train_count/total_len))
+    # print('Validation COUNT: {} Validation PCT: {}'.format(val_count, val_count/total_len))
+    # print('Test COUNT: {} Test PCT: {}'.format(test_count, test_count/total_len))
+    
+    # print('\nUsers in each split:')
+    # print('Training: ', ratings_train.select('userId').distinct().count())
+    # print('Validation: ', ratings_val.select('userId').distinct().count())
+    # print('Test: ', ratings_test.select('userId').distinct().count())
+    
+    # print('\nMovies in each split:')
+    # print('Training: ', ratings_train.select('movieId').distinct().count())
+    # print('Validation: ', ratings_val.select('movieId').distinct().count())
+    # print('Test: ', ratings_test.select('movieId').distinct().count())
+    
+    # '---FOR DEBUGGING---'
+    
     if local_save == True:
         filepath = ''
     
@@ -115,7 +146,7 @@ if __name__ == "__main__":
     # Create the spark session object
     spark = SparkSession.builder.appName('part1').getOrCreate()
 
-    local_save = False # Set = True for debugging on local machine
+    local_save = True # Set = True for debugging on local machine
     
     full_data = False
     size = '-small' if full_data==False else ''
